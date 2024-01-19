8a41a5fe514889873f823e9dde175459c1398665
Meikaku77 <147025537+Meikaku77@users.noreply.github.com>
2024-01-19 06:28:24 +0100

    insta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {NavbarComponent}  from './components/NavbarComponent'
 import { CardComponent } from './components/CardComponent'
+import {Card} from 'keep-react'
+import { InstagramLogo } from 'phosphor-react'
 
 
 export const App = () => {
@@ -11,6 +13,12 @@ export const App = () => {
   
     <footer className="flex bg-orange-400 p-6 mt-7 justify-around align-middle">
       <p className="font-bold text-lg text-white">Web DIY</p>
+      <Card.Link
+            className="flex items-center justify-center px-3 py-1"
+            icon={<InstagramLogo size={24} color="red" weight="fill" />}
+            href="https://www.instagram.com/ismaelberoncastano?igsh=dDdkZ3lzZHIxajRz"
+            title="Instagram"
+          />
       <p className="font-bold text-lg text-white">Ja tens el meu contacte!</p>
     </footer>
     </div>
--- src/components/PopContact.jsx
+++ src/components/PopContact.jsx
@@ -1,6 +1,7 @@
 
 
-import { Button, Popover } from 'keep-react'
+import { Button, Popover, Card } from 'keep-react'
+import {InstagramLogo} from 'phosphor-react'
 
 export const PopoverComponent = () => {
   return (
@@ -8,6 +9,11 @@ export const PopoverComponent = () => {
       <Popover.Title>Informació de contacte</Popover.Title>
       <Popover.Description>
       Pots escriure'm a <span>ismaelberoncastano@gmail.com</span>
+      <Card.Link
+            className="flex items-center justify-center px-3 py-1"
+            icon={<InstagramLogo size={24} color="red" weight="fill" />}
+            href="https://www.instagram.com/ismaelberoncastano?igsh=dDdkZ3lzZHIxajRz"
+          />
       </Popover.Description>
       <Popover.Action>
         <Button size="xs" className='bg-orange-600 text-white font-bold hover:bg-orange-700'>Contacte</Button>
